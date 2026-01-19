89048a384760d2b162e379492d5c8cacd9f37e96
Gavin400 <perisgavin@gmail.com>
2026-01-19 23:17:10 +0100

    Message(Migrations and Repository Logic)

--- API/Data/MessageRepository.cs
+++ API/Data/MessageRepository.cs
@@ -5,14 +5,18 @@ using API.Entities;
 using API.Extensions;
 using API.Helpers;
 using API.Interfaces;
+using CloudinaryDotNet.Actions;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Data;
 
 public class MessageRepository(AppDbContext context) : IMessageRepository
 {
+    public void AddGroup(Entities.Group group)
+    {
+        context.Groups.Add(group);
+    }
 
-    
     public void AddMessage(Message message)
     {
         context.Messages.Add(message);
@@ -23,9 +27,29 @@ public class MessageRepository(AppDbContext context) : IMessageRepository
         context.Messages.Remove(message);
     }
 
+    public async Task<Connection?> GetConnection(string connectionId)
+    {
+        return await context.Connections.FindAsync(connectionId); 
+    }
+
+    public async Task<Entities.Group?> GetGroupForConnection(string connectionId)
+    {
+        return await context.Groups
+            .Include(x => x.Connections)
+            .Where(x => x.Connections.Any(c => c.ConnectionId == connectionId))
+            .FirstOrDefaultAsync();
+    }
+
     public async Task<Message?> GetMessage(string messageId)
     {
         return await context.Messages.FindAsync(messageId);
+    }
+
+    public async Task<Entities.Group?> GetMessageGroup(string groupName)
+    {
+        return await context.Groups
+            .Include(x => x.Connections)
+            .FirstOrDefaultAsync(x => x.Name == groupName);
     }
 
     // public async Task<Group?> GetMessageGroup(string groupName)
@@ -70,6 +94,13 @@ public class MessageRepository(AppDbContext context) : IMessageRepository
             .OrderBy(x=> x.MessageSent)
             .Select(MessageExtensions.ToDtoProjection())
             .ToListAsync();
+    }
+
+    public async Task RemoveConnection(string connectionId)
+    {
+        await context.Connections
+            .Where(x => x.ConnectionId == connectionId)
+            .ExecuteDeleteAsync();
     }
 
     public async Task<bool> SaveAllAsync()
